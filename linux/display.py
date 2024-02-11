263580c84b4d91d7a59ae9d5f87c16d2ba4103da
minkberry <chaifoxes@gmail.com>
2024-02-11 21:50:05 +0100

    Hopefully fixed crashes.

--- linux/display.py
+++ linux/display.py
@@ -1,3 +1,5 @@
+import time
+
 from luma.core.interface.serial import i2c, spi, pcf8574
 from luma.core.interface.parallel import bitbang_6800
 from luma.core.render import canvas
@@ -10,13 +12,18 @@ def run(update_func):
 	font = ImageFont.truetype('PixelOperator.ttf', 16)
 	icons = ImageFont.truetype('lineawesome-webfont.ttf', 16)
 
-	serial = i2c(port=1, address=0x3C)
-	device = sh1106(serial, width=128, height=64, rotate=2)
-
 	while True:
-		with canvas(device) as draw:
-			draw.rectangle(device.bounding_box, outline="black", fill="black")
-			update_func()
+		try:
+			serial = i2c(port=1, address=0x3C)
+			device = sh1106(serial, width=128, height=64, rotate=2)
+
+			while True:
+				with canvas(device) as draw:
+					draw.rectangle(device.bounding_box, outline="black", fill="black")
+					update_func()
+		except (IOError, OSError) as e:
+			print("i/o error caught: " + str(e))
+			time.sleep(1)
 
 def draw_text(x, y, text):
 	draw.text((x, y), text, fill=255, font=font)
